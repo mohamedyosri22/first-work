9a87460a7e09bfad7d0fc4b8d0cb055d7b0e30c6
mhmdyouali <mhmd.you@yahoo.com>
2022-12-09 05:31:08 +0200

    azure db connection

---
+++ src/main/java/com/spring/SpringBootWork.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWork {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWork.class, args);
+	}
+
+}
